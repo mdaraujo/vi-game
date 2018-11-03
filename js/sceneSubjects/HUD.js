e8c13382dd3d79eb23353f73d300276ddb7c4251
Miguel Araújo <mdaraujo@users.noreply.github.com>
2018-11-03 01:18:58 +0000

    HUD on html instead of TextGeometry

--- js/sceneSubjects/HUD.js
+++ js/sceneSubjects/HUD.js
@@ -1,61 +1,21 @@
 function HUD(scene) {
 
-    const size = 7, height = 1;
-
-    var killCount;
-    var mesh = new THREE.Mesh();
-    var geometry;
-    var font;
+    var killCounter;
+    var killsElement;
 
     this.init = function () {
-
-        killCount = 0;
-
-        var loader = new THREE.FontLoader();
-        loader.load('fonts/gentilis_regular.typeface.json', function (fontLoaded) {
-            font = fontLoaded;
-
-            geometry = new THREE.TextGeometry(killCount + "", {
-                font: font,
-                size: size,
-                height: height
-            });
-
-            var material = new THREE.MeshPhongMaterial({
-                color: "#148114",
-                shininess: 10
-            });
-
-            mesh = new THREE.Mesh(geometry, material);
-
-            mesh.position.x = 0;
-            mesh.position.y = height / 2;
-            mesh.position.z = -80;
-            mesh.rotation.x = camera.rotation.x;
-            mesh.rotation.y = camera.rotation.y;
-
-            scene.add(mesh);
-        });
-
+        killCounter = 0;
+        killsElement = document.getElementById("killCounter");
+        killsElement.innerHTML = killCounter;
     }
 
     this.update = function (time) {
-        mesh.rotation.x = camera.rotation.x;
-        mesh.rotation.y = camera.rotation.y;
+
     }
 
     this.addKill = function () {
-        killCount++;
-        updateMesh();
+        killCounter++;
+        killsElement.innerHTML = killCounter;
     }
 
-    function updateMesh() {
-        geometry = new THREE.TextGeometry(killCount + "", {
-            font: font,
-            size: size,
-            height: height
-        });
-
-        mesh.geometry = geometry;
-    }
 }
