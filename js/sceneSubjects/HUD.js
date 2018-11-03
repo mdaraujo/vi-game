3f427fb09930f3d85a97351af8076c5cf0facb53
Miguel Araújo <mdaraujo@users.noreply.github.com>
2018-11-03 01:43:09 +0000

    timer on HUD

--- js/sceneSubjects/HUD.js
+++ js/sceneSubjects/HUD.js
@@ -3,14 +3,29 @@ function HUD(scene) {
     var killCounter;
     var killsElement;
 
+    var timeElement;
+    var nextTimeUpdate;
+    var date;
+
     this.init = function () {
         killCounter = 0;
+
         killsElement = document.getElementById("killCounter");
         killsElement.innerHTML = killCounter;
+
+        timeElement = document.getElementById("time");
+        nextTimeUpdate = 0;
+
     }
 
     this.update = function (time) {
-
+        if (time >= nextTimeUpdate) {
+            date = new Date(null);
+            date.setSeconds(time);
+            var timeString = date.toISOString().substr(11, 8);
+            timeElement.innerHTML = timeString;
+            nextTimeUpdate += 1;
+        }
     }
 
     this.addKill = function () {
